cbac1e9e90c089f188d646c1cf6bdc65bf2d10d3
Jeremy Shipman <jeremy@burnbright.net>
2012-08-16 11:35:09 +1200

    Added filter to check that product is ShowInSearch. Removed unused code.

--- code/SearchPage.php
+++ code/SearchPage.php
@@ -1,10 +1,18 @@
 <?php
-
 /**
  * SearchPage_Controller is a stand-alone page for searching products,
  * reachable by default at yoursite.com/search.
  * 
  * Pass a 'search' get parameter in the url to perform the search.
+ * 
+ * TODO:
+ * 	sort results by popularity, newest
+ * 	choose number of results
+ * 	join with other dataobjects for fuller search
+ * 	closer matches are better eg 'sam' vs 'samoa'
+ * 	search module
+ * 		fuzzy search
+ * 		mispelling
  * 
  */
 class SearchPage_Controller extends Page_Controller{
@@ -19,59 +27,25 @@ class SearchPage_Controller extends Page_Controller{
 	}
 	
 	function index(){
-		
 		$phrase = null;
-		
 		if(!$this->request->getVar('search')){
 			return array();
 		}
 		$phrase = Convert::raw2sql($this->request->getVar('search'));
-		
 		$filters = array(
 			"\"Title\" LIKE '%$phrase%'",
-			//Subshop "\"Title\" LIKE '%$phrase%'",
-			//Artist?
-			//Tag
+			"\"ShowInSearch\" = 1"
 		);
-	
-		//sort by popularity, newer
-		//join with stores and tags
-		//closer matches are better eg 'sam' vs 'samoa'
-		//search module
-			//fuzzy search
-			//mispellings
 		$page = ((int)$this->request->getVar('page')) ? (int)$this->request->getVar('page') : 1;
 		$limit = 16;
-		
 		$start = $limit + 1;
 		$paging = ($page-1)*$limit.",".$start;
-		
 		$filter = implode(" AND ",$filters);
-		
 		$joins = array(
-			"INNER JOIN \"SiteTree\" ON \"Product\".\"ID\" = \"SiteTree\".\"ID\"",
-			//"LEFT JOIN \"SubShop\" ON \"Product\".\"SubShopID\" = \"SubShop\".\"ID\"",
-			//"LEFT JOIN \"Product_Tags\" ON \"Product\".\"ID\" = \"Product_Tags\".\"ProductID\"",
-			//"LEFT JOIN \"Tag\" ON \"Product_Tags\".\"TagID\" = \"Tag\".\"ID\""
+			"INNER JOIN \"SiteTree\" ON \"Product\".\"ID\" = \"SiteTree\".\"ID\""
 		);
-		
 		$sort = "\"NumberSold\" ASC,\"Created\" DESC";
-		
-		//query
-		/*
-		$query = new SQLQuery();
-		$query->select = array(
-			'"Product".*',
-			//'"Tag"."Title" AS "TagTitle"',
-			//'"SubShop"."Title" AS "SubShopTitle"'
-		);
-		$query->from = array_merge(array('Product'),$joins);
-		$query->where = $filters;
-		$results = singleton('Product')->buildDataObjectSet($query->execute());
-		*/
-		
 		$results = DataObject::get("Product",$filter,$sort,"",$paging);
-		
 		$link = $this->Link()."?search=".urlencode($phrase);
 		$morelink = $previouslink = null;
 		if($page > 1){
@@ -81,7 +55,6 @@ class SearchPage_Controller extends Page_Controller{
 			$morelink = $link."&page=".($page+1);
 			$results->pop();
 		}
-
 		return array(
 			'Phrase' => Convert::raw2xml($phrase),
 			'Results' => $results,
